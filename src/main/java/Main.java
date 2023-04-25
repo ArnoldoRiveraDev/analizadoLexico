66997c880fde44c73ae461ea86830f4ebfcf47ef
arnoldoriveradev <arnoldodevh7@gmail.com>
2023-04-24 21:43:19 -0600

    Se agregaron nuevas cadenas para testear el codigo.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,18 @@ import java.util.List;
  * @Author
  * Arnoldo Rivera Mimbela
  *
+ * cadenas con las que puede testear profesor:
+ * if (x == 3) { y = 2; }
+ * for (i = 0; i < 10; i++) { System.out.println(i); }
+ * <, ;, ++, {, ;, } y ()
+ * x = "Hola, mundo!";
+ * y = 2.5 + x;
+ *
  */
 public class Main {
 
     public static void main(String[] args) {
-        String codigoFuente = "int x = 8 + 3;";
+        String codigoFuente = "for (i = 0; i < 10; i++) { System.out.println(i); }";
         AnalizadorLexico analizador = new AnalizadorLexico(codigoFuente);
         List<Token> tokens = analizador.analizar();
         for (Token token : tokens) {
